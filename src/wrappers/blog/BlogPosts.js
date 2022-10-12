12f4fdd4f0fe0fc451c0f2439d9dc5f695766c9b
Devender <lucdev93@gmail.com>
2022-10-12 18:19:30 +0530

    blog change with news

--- src/wrappers/blog/BlogPosts.js
+++ src/wrappers/blog/BlogPosts.js
@@ -1,14 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useState, useEffect  } from "react";
 import { Link } from "react-router-dom";
+
 const BlogPosts = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(()=>{
+const dataget=async()=>{
+  try {
+   const response= await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-09-12&sortBy=publishedAt&apiKey=6c724583b6e74a349f7e512e42fa41a4`);
+   if(!response.ok){
+    throw new Error(
+      `This is an HTTP error: The status is ${response.status}`
+    );
+   }
+   let realdata= await response.json();
+   setData(realdata);
+   setError(null);
+  } 
+  catch (error) {
+    setError(error.message);
+    setData(null);
+  }
+  finally{
+    setLoading(false)
+  }
+}
+dataget()
+  },[])
+ 
+
   return (
     <Fragment>
+       {loading && <div>A moment please...</div>}
+       {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+       {data &&
+          data.articles.map(({ title,description,urlToImage,publishedAt}) => (
       <div className="col-lg-6 col-md-6 col-sm-12">
+      
         <div className="blog-wrap-2 mb-30">
           <div className="blog-img-2">
             <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
               <img
-                src={process.env.PUBLIC_URL + "/assets/img/blog/blog-9.jpg"}
+                src={urlToImage}
                 alt=""
               />
             </Link>
@@ -16,7 +52,7 @@ const BlogPosts = () => {
           <div className="blog-content-2">
             <div className="blog-meta-2">
               <ul>
-                <li>22 April, 2020</li>
+                <li>publishedAt</li>
                 <li>
                   <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
                     4 <i className="fa fa-comments-o" />
@@ -26,12 +62,11 @@ const BlogPosts = () => {
             </div>
             <h4>
               <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                Lorem ipsum blog post
+               {title}
               </Link>
             </h4>
             <p>
-              Aenean sollicitudin, lorem quis on endum uctor nisi elitod the
-              cona sequat ipsum, necas sagittis sem natoque nibh id penatibus
+             {description}
             </p>
             <div className="blog-share-comment">
               <div className="blog-btn-2">
@@ -65,316 +100,7 @@ const BlogPosts = () => {
           </div>
         </div>
       </div>
-      <div className="col-lg-6 col-md-6 col-sm-12">
-        <div className="blog-wrap-2 mb-30">
-          <div className="blog-img-2">
-            <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/img/blog/blog-8.jpg"}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className="blog-content-2">
-            <div className="blog-meta-2">
-              <ul>
-                <li>22 April, 2020</li>
-                <li>
-                  <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                    4 <i className="fa fa-comments-o" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <h4>
-              <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                New collection of our shop
-              </Link>
-            </h4>
-            <p>
-              Aenean sollicitudin, lorem quis on endum uctor nisi elitod the
-              cona sequat ipsum, necas sagittis sem natoque nibh id penatibus
-            </p>
-            <div className="blog-share-comment">
-              <div className="blog-btn-2">
-                <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                  read more
-                </Link>
-              </div>
-              <div className="blog-share">
-                <span>share :</span>
-                <div className="share-social">
-                  <ul>
-                    <li>
-                      <a className="facebook" href="//facebook.com">
-                        <i className="fa fa-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="twitter" href="//twitter.com">
-                        <i className="fa fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="instagram" href="//instagram.com">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-lg-6 col-md-6 col-sm-12">
-        <div className="blog-wrap-2 mb-30">
-          <div className="blog-img-2">
-            <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/img/blog/blog-7.jpg"}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className="blog-content-2">
-            <div className="blog-meta-2">
-              <ul>
-                <li>22 April, 2020</li>
-                <li>
-                  <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                    4 <i className="fa fa-comments-o" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <h4>
-              <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                Ipsum blog post two
-              </Link>
-            </h4>
-            <p>
-              Aenean sollicitudin, lorem quis on endum uctor nisi elitod the
-              cona sequat ipsum, necas sagittis sem natoque nibh id penatibus
-            </p>
-            <div className="blog-share-comment">
-              <div className="blog-btn-2">
-                <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                  read more
-                </Link>
-              </div>
-              <div className="blog-share">
-                <span>share :</span>
-                <div className="share-social">
-                  <ul>
-                    <li>
-                      <a className="facebook" href="//facebook.com">
-                        <i className="fa fa-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="twitter" href="//twitter.com">
-                        <i className="fa fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="instagram" href="//instagram.com">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-lg-6 col-md-6 col-sm-12">
-        <div className="blog-wrap-2 mb-30">
-          <div className="blog-img-2">
-            <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/img/blog/blog-6.jpg"}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className="blog-content-2">
-            <div className="blog-meta-2">
-              <ul>
-                <li>22 April, 2020</li>
-                <li>
-                  <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                    4 <i className="fa fa-comments-o" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <h4>
-              <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                New shop collection
-              </Link>
-            </h4>
-            <p>
-              Aenean sollicitudin, lorem quis on endum uctor nisi elitod the
-              cona sequat ipsum, necas sagittis sem natoque nibh id penatibus
-            </p>
-            <div className="blog-share-comment">
-              <div className="blog-btn-2">
-                <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                  read more
-                </Link>
-              </div>
-              <div className="blog-share">
-                <span>share :</span>
-                <div className="share-social">
-                  <ul>
-                    <li>
-                      <a className="facebook" href="//facebook.com">
-                        <i className="fa fa-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="twitter" href="//twitter.com">
-                        <i className="fa fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="instagram" href="//instagram.com">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-lg-6 col-md-6 col-sm-12">
-        <div className="blog-wrap-2 mb-30">
-          <div className="blog-img-2">
-            <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/img/blog/blog-5.jpg"}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className="blog-content-2">
-            <div className="blog-meta-2">
-              <ul>
-                <li>22 April, 2020</li>
-                <li>
-                  <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                    4 <i className="fa fa-comments-o" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <h4>
-              <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                Lorem blog post four
-              </Link>
-            </h4>
-            <p>
-              Aenean sollicitudin, lorem quis on endum uctor nisi elitod the
-              cona sequat ipsum, necas sagittis sem natoque nibh id penatibus
-            </p>
-            <div className="blog-share-comment">
-              <div className="blog-btn-2">
-                <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                  read more
-                </Link>
-              </div>
-              <div className="blog-share">
-                <span>share :</span>
-                <div className="share-social">
-                  <ul>
-                    <li>
-                      <a className="facebook" href="//facebook.com">
-                        <i className="fa fa-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="twitter" href="//twitter.com">
-                        <i className="fa fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="instagram" href="//instagram.com">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-lg-6 col-md-6 col-sm-12">
-        <div className="blog-wrap-2 mb-30">
-          <div className="blog-img-2">
-            <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/img/blog/blog-4.jpg"}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className="blog-content-2">
-            <div className="blog-meta-2">
-              <ul>
-                <li>22 April, 2020</li>
-                <li>
-                  <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                    4 <i className="fa fa-comments-o" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <h4>
-              <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                Ipsum blog post five
-              </Link>
-            </h4>
-            <p>
-              Aenean sollicitudin, lorem quis on endum uctor nisi elitod the
-              cona sequat ipsum, necas sagittis sem natoque nibh id penatibus
-            </p>
-            <div className="blog-share-comment">
-              <div className="blog-btn-2">
-                <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
-                  read more
-                </Link>
-              </div>
-              <div className="blog-share">
-                <span>share :</span>
-                <div className="share-social">
-                  <ul>
-                    <li>
-                      <a className="facebook" href="//facebook.com">
-                        <i className="fa fa-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="twitter" href="//twitter.com">
-                        <i className="fa fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a className="instagram" href="//instagram.com">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+       ))}
     </Fragment>
   );
 };
